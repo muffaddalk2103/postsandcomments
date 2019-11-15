8eac6ddbe393f785a24ada4a1926ed7f31510574
muffaddalk2103 <57375125+muffaddalk2103@users.noreply.github.com>
2019-11-14 19:53:51 -0500

    hello world

---
+++ postsandcomments/src/main/java/com/loyaltyone/postsandcomments/PostsandcommentsApplication.java
@@ -0,0 +1,13 @@
+package com.loyaltyone.postsandcomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsandcommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsandcommentsApplication.class, args);
+	}
+
+}
---
+++ postsandcomments/src/test/java/com/loyaltyone/postsandcomments/PostsandcommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loyaltyone.postsandcomments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsandcommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
